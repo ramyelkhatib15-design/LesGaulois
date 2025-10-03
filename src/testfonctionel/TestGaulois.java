1fbb369c3362200f6594ca33ff9905a08bfd91ab
Ramy Elkhatib <lkr5035a@u6-100-20d.univ-tlse3.fr>
2025-10-03 09:44:23 +0200

    nouveau

---
+++ src/testfonctionel/TestGaulois.java
@@ -0,0 +1,16 @@
+package testfonctionel;
+
+import personnages.Gaulois;
+
+public class TestGaulois {
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("Asterix",8);
+		Gaulois obelix = new Gaulois("Obelix",16);
+		asterix.parler("Bonjour Obélix.");
+		obelix.parler("Bonjour Astérix. Ca te dirais d'aller chasser des sangliers ?");
+		asterix.parler("Oui très bonne idée.");
+		
+	}
+	
+
+}
